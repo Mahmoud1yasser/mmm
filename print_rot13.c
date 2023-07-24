313454436d966120cc2d015094d59464a4da7e27
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-07-24 11:01:43 -0700

    abdo

--- print_rot13.c
+++ print_rot13.c
@@ -1,16 +1,19 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
   * print_rot13 - encodes a string into rot13.
   * @R: string to convert
   * Return: size the output text
-  */
+*/
+
 int print_rot13(va_list R)
 {
 	int j, i, count = 0;
 	char *r;
-	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLM nopqrstuvwxyzabcdefghijklm";
 
 	r = va_arg(R, char *);
 	if (r == NULL)
@@ -18,26 +21,24 @@ int print_rot13(va_list R)
 	for (j = 0; r[j] != '\0'; j++)
 	{
 		char c = r[j];
+
 		if (c >= 'A' && c <= 'Z')
 		{
-// Uppercase letter, use the input array
-            i = c - 'A';
-            _putchar(output[i]);
-            count++;
-        }
-        else if (c >= 'a' && c <= 'z')
-        {
-            // Lowercase letter, use the input array with offset
-            i = c - 'a' + 26;
-            _putchar(output[i]);
-            count++;
-        }
-        else
-        {
-            // Not a letter, print as is
-            _putchar(c);
-            count++;
-        }
-    }
-    return count;
+			i = c - 'A';
+			_putchar(output[i]);
+			count++;
+		}
+		else if (c >= 'a' && c <= 'z')
+		{
+			i = c - 'a' + 26;
+			_putchar(output[i]);
+			count++;
+		}
+		else
+		{
+			_putchar(c);
+			count++;
+		}
+	}
+	return (count);
 }
