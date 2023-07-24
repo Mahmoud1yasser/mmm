cfd2518228334766624a8e5b0bee88e6f7b44718
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-07-24 11:35:44 -0700

    abdo

--- print_bin.c
+++ print_bin.c
@@ -3,12 +3,15 @@
 #include "main.h"
 /**
 * print_bin - convert to binary
+*
 * @b: decimal number
+*
 * Return: number of chars printed(sucess)
 */
+
 int print_bin(va_list b)
 {
-	unsigned int lenght, powten, j;
+	unsigned int len, pten, j;
 	unsigned int digit, n, number;
 	int count = 0;
 
@@ -16,22 +19,22 @@ int print_bin(va_list b)
 	if (n != 0)
 	{
 		number = n;
-		lenght = 0;
+		len = 0;
 		while (number != 0)
 		{
 			number /= 2;
-			lenght++;
+			len++;
 		}
-		powten = 1;
-		for (j = 1; j <= lenght - 1; j++)
-			powten *= 2;
-		for (j = 1; j <= lenght; j++)
+		pten = 1;
+		for (j = 1; j <= len - 1; j++)
+			pten *= 2;
+		for (j = 1; j <= len; j++)
 		{
-			digit = n / powten;
+			digit = n / pten;
 			_putchar(digit + '0');
 			count++;
-			n -= digit * powten;
-			powten /= 2;
+			n -= digit * pten;
+			pten /= 2;
 		}
 	}
 	else
